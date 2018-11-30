a2c72b2c4336eca45f3c91292c890829a786559d
Klas Wärnå <klas.warna@gmail.com>
2018-11-30 13:06:13 +0000

    fixed wrong api-key-file

--- config/apikeys.php
+++ config/apikeys.php
@@ -1,17 +1,9 @@
 <?php
 /**
- * Config file for apikeys.
+ * Config file api:s
  */
 return [
-    "services" => [
-        "apikeys" => [
-            "shared" => true,
-            "callback" => function () {
-                // Load the configuration files
-                $cfg = $this->get("configuration");
-                $config = $cfg->load("apikeys.php");
-                return $config;
-            }
-        ]
-    ]
+    "darkSky"   => "8fc8b212a2c3531bdd01693e25e1442f",
+    "openStreetMap"   => "email=klas.warna@gmail.com",
+    "ipCheck" => "372e5a3587b9a517d3c3d9369e1fcd94",
 ];
