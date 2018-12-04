74d7ed891d99306314b1cf2783aae65263bb5a27
klaswarna <klas.warna@gmail.com>
2018-12-04 19:33:44 +0000

    nytt experiment

--- test/config.php
+++ test/config.php
@@ -5,18 +5,18 @@
 /**
  * Define essential Anax paths, end with /
  */
-define("ANAX_INSTALL_PATH", realpath(__DIR__ . "/.."));
+//define("ANAX_INSTALL_PATH", realpath(__DIR__ . "/.."));
 /**
  * Include autoloader.
  */
-require ANAX_INSTALL_PATH . "/vendor/autoload.php";
+//require ANAX_INSTALL_PATH . "/vendor/autoload.php";
 /**
  * Use $di as global identifier (used in views by viewhelpers).
  */
-$di = null;
+//$di = null;
 /**
  * Include others.
  */
-foreach (glob(__DIR__ . "/Mock/*.php") as $file) {
-    require $file;
-}
+//foreach (glob(__DIR__ . "/Mock/*.php") as $file) {
+//    require $file;
+//}
